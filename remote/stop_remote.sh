b88fc39000204ab01d656b838cb00ff2e31b144f
Kirishi Hackerspace <sw3n32@gmail.com>
2025-01-13 21:33:35 +0300

    Add files via upload

---
+++ remote/stop_remote.sh
@@ -0,0 +1,14 @@
+echo "Stop Eye..."
+eye=$(ps |grep eye.sh|awk '{print $1}')
+echo $eye
+kill -9 $eye
+sleep 1
+echo "Stop WEB-Server..."
+tiny=$(pidof tiny)
+echo $tiny
+trap 'kill -9 $tiny' SIGINT SIGTERM EXIT
+sleep 1
+echo "STOP web-camera"
+sleep 1
+#kill -9 $(pidof fswebcam)
+echo "all process killed, check ps"
