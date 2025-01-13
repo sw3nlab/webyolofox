4a69ed3c5ee4b2a2cb67e0086ade09ae788638f0
Kirishi Hackerspace <sw3n32@gmail.com>
2025-01-13 18:25:42 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Start Web-cam 1 FPS..."
 fswebcam -q -l 1 -d v4l2:/dev/video0 image.jpg &
 sleep 1
 echo "Web server starting..."
-/var/www/tiny 8080 >/dev/null&
+webserver/tiny 8080 >/dev/null&
 sleep 1
 echo "Started on http://localhost:8080/"
 sleep 1
-echo "Starting yolo..."
+echo "Starting web-detector..."
 sleep 1
-/var/www/yolov5/yolo.sh >/dev/null&
+./yolo.sh >/dev/null&
 sleep 1
-echo "yolo started..."
+echo "Web-detector started..."
