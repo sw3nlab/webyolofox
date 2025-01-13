76bd25721b1ac438523a1aa231002f7be1198f01
Kirishi Hackerspace <sw3n32@gmail.com>
2025-01-13 14:04:26 +0300

    Add files via upload

---
+++ remote/start_remote.sh
@@ -0,0 +1,10 @@
+echo "Web server starting..."
+../webserver/tiny 8080 >/dev/null&
+sleep 1
+echo "Started on http://localhost:8080/"
+sleep 1
+echo "Starting REMOTE detector..."
+sleep 1
+/var/www/yolov5/yolo.sh >/dev/null&
+sleep 1
+echo "Remote Detector started..."
