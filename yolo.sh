8d7edfb3d4e11fb235db235124cd179e3ada8de9
Kirishi Hackerspace <sw3n32@gmail.com>
2025-01-13 18:28:31 +0300

    Update yolo.sh

--- yolo.sh
+++ yolo.sh
@@ -1,7 +1,5 @@
 while true
 do
-#wget -q -O image.jpg http://192.168.2.176/image.jpg
-#sleep 1
-/var/www/yolov5/yolov5 /var/www/yolov5/model/yolov5.rknn image.jpg
+./yolov5 model/yolov5.rknn image.jpg
 sleep 1
 done
