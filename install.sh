bf86f1421e6d90158f453944e0150e51c662812a
Kirishi Hackerspace <sw3n32@gmail.com>
2025-01-14 11:11:18 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
-echo "Install webyolofox..."
+echo "Install webyolofox... "
+echo "chmod files +"
 chmod a+x detect.sh start.sh stop.sh eye eye.sh webserver/tiny remote/eye remote/eye.sh remote/start_remote.sh remote/stop_remote.sh 
+echo "copy libs +"
+cp lib/libgd.so.3 lib/libpng16.so.16 lib/libfontconfig.so.1 lib/libjpeg.so.8 /usr/lib/.
+mv fswebcam /usr/sbin/.
+echo "installation complete"
+echo "Please check webcam and run:"
+echo "For remote webcam: remote/star_remote.sh"
+echo "For usb-webcam: ./start.sh"
