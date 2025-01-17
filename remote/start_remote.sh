508512c7af84dc5e9a48e295afe7426fcaa1b38c
Kirishi Hackerspace <sw3n32@gmail.com>
2025-01-17 17:37:14 +0300

    Update start_remote.sh

--- remote/start_remote.sh
+++ remote/start_remote.sh
@@ -1,7 +1,7 @@
 echo "Web server starting..."
 ../webserver/tiny 8080 >/dev/null&
 sleep 1
-echo "Started on http://localhost:8080/"
+echo "Tiny Started on port 8080"
 sleep 1
 echo "Starting REMOTE detector..."
 sleep 1
